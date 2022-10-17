060ddec8a68a529cc3b860b6d30445851dde8454
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-10-18 00:53:05 +0900

    게시판 테이블

---
+++ src/main/java/com/example/board/domain/Board.java
@@ -0,0 +1,15 @@
+package com.example.board.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Board {
+
+
+
+}
