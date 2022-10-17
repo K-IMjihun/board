0ebbee6c410c09f45facef65100e8cc5f2cf7294
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-10-18 01:18:16 +0900

    게시판 VO에 Time 추가

--- src/main/java/com/example/board/controller/vo/BoardVO.java
+++ src/main/java/com/example/board/controller/vo/BoardVO.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data // Getter, Setter, RequiredArgsConstructor
 @NoArgsConstructor // 기본 생성자
 @AllArgsConstructor // 필드 전체를 가지고 있는 생성자
 public class BoardVO {
 
+  private LocalDate createTime;
+  private LocalDate updateTime;
   private String subject;
   private String content;
 
