437d27371afc9980c157152aabd8f577694c5044
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-10-18 01:55:25 +0900

    Repository 어노테이션

--- src/main/java/com/example/board/repository/BoardRepository.java
+++ src/main/java/com/example/board/repository/BoardRepository.java
@@ -1,16 +1,16 @@
 package com.example.board.repository;
 
-import com.example.board.controller.dto.BoardSearchDTO;
 import com.example.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-  List<Board> findAllSubjectContainsOrContentContainsOrModifiedAtIsLessThanEqual(
+  List<Board> findAllBySubjectContainsOrContentContainsOrModifiedAtIsLessThanEqual(
       String subject, String content, LocalDate modifiedAt);
 
 }
