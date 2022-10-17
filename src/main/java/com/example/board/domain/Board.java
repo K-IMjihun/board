3c1e464611e34a07a3a7aa7583c9f63d96c59340
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-10-18 01:24:02 +0900

    Board Builder

--- src/main/java/com/example/board/domain/Board.java
+++ src/main/java/com/example/board/domain/Board.java
@@ -1,14 +1,15 @@
 package com.example.board.domain;
 
 import com.example.board.domain.common.BaseTimeEntity;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class Board extends BaseTimeEntity {
 
   private String subject;
